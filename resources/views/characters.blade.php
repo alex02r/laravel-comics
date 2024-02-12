e86e17440a604872471d735f55410ff7cd0cbd3e
alex02r <alex.giochi02@gmail.com>
2024-02-12 16:52:18 +0100

    [UPGRADE] Creazione file characters.blade.php

---
+++ resources/views/characters.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CHARACHTERS</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    @include('partials.header')
+    <main class="bg-dark text-white">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12 py-5">
+                    <h1> PAGE CHARACTERS</h1>
+                    <h2> in allestimento</h2>
+                </div>
+            </div>
+        </div>
+    </main>
+    @include('partials.footer')
+</body>
+</html>
