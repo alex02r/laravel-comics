5808ea04fe874f0a6e814f1e850e279039639638
alex02r <alex.giochi02@gmail.com>
2024-02-12 14:32:24 +0100

    [UPGRADE] Aggiunto stile per la nav list

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -21,6 +21,18 @@
             </div>
             <div class="col-10">
                 {{-- nav menu --}}
+                <ul class="my-list">
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
             </div>
         </div>
     </div>
