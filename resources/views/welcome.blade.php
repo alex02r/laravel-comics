fb8023d60ea74a85cd1313b9f83af5383608bbf2
alex02r <alex.giochi02@gmail.com>
2024-02-12 15:17:34 +0100

    [UPGRADE] Aggiunta sezione lista dc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,42 @@
                     <span class="btn btn-primary py-1 px-4 rounded-1">LOAD MORE</span>
                 </div>
             </div>
-
+        </div>
+        <div class="bg-primary">
+            <div class="container py-5">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-auto">
+                        <div class="item">
+                            <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" alt="item">
+                            <h6>DIGITAL COMICS</h6>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="item">
+                            <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" alt="item">
+                            <h6>DC MERCHANDISE</h6>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="item">
+                            <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" alt="item">
+                            <h6>SUBSCRIPTION</h6>
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <div class="item">
+                            <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" alt="item">
+                            <h6>COMIC SHOP LOCATOR</h6>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <div class="item">
+                            <img src="{{Vite::asset('resources/images/buy-dc-power-visa.svg')}}" alt="item">
+                            <h6>DC POWER VISA</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 
