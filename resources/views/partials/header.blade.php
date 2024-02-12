57f854e5618e0d2bfa11f50bce3a450b5f7453a8
alex02r <alex.giochi02@gmail.com>
2024-02-12 14:02:58 +0100

    [UPGRADE] Stilizzata prima parte dell'header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    
+    <div class="bg-primary">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-6 col-md-4 col-lg-3">
+                    <span class="text-white">DC POWER&#8480; VISA&#174;</span>
+                </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <span class="text-white">ADDITIONAL DC SITES &#9662;</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
