e7f1783772934a58e99b70f96b01f9a50558acc6
alex02r <alex.giochi02@gmail.com>
2024-02-12 15:20:27 +0100

    [UPGRADE] Struttura HTML per il footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,12 @@
 <footer>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                {{-- list side --}}
+            </div>
+            <div class="col-6">
+                {{-- img side --}}
+            </div>
+        </div>
+    </div>
 </footer>
