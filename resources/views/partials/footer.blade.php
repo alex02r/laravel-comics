291744c8a2cb728f5ca7046e603300a6490faa6b
alex02r <alex.giochi02@gmail.com>
2024-02-12 15:30:33 +0100

    [UPGRADE] Aggiunti i background al footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,12 @@
 <footer>
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-between">
             <div class="col-4">
                 {{-- list side --}}
             </div>
             <div class="col-6">
                 {{-- img side --}}
+                <div class="bg-footer"></div>
             </div>
         </div>
     </div>
