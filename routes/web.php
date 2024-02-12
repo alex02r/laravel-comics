a179bf8fa43efd6eec6d0ab83e2df426b96ef546
alex02r <alex.giochi02@gmail.com>
2024-02-12 16:45:20 +0100

    [FIX] Cambiato il nome di welcome

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $comics = config('comics');
-    return view('welcome', compact('comics'));
+    return view('comics', compact('comics'));
 });
