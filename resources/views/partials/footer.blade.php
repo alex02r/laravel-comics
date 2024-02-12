e7e0599331e8c7beab933b102b2b861189d8181a
alex02r <alex.giochi02@gmail.com>
2024-02-12 15:44:10 +0100

    [UPGRADE] Lista footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,54 @@
     <div class="container">
         <div class="row justify-content-between">
             <div class="col-4">
-                {{-- list side --}}
+                <div class="lists-group">
+                    <div class="footer-list">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Collectibles</li>
+                            <li>Videos</li>
+                            <li>Fans</li>
+                            <li>News</li>
+                            <li>Shop</li>
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div class="footer-list">
+                        <h4>DC</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Collectibles</li>
+                            <li>Videos</li>
+                            <li>Fans</li>
+                            <li>News</li>
+                            <li>Shop</li>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <div class="footer-list">
+                        <h4>SITES</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="col-6">
                 {{-- img side --}}
