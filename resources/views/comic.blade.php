19907ca7e9d6d80775d1b899737c16f64713e4d9
alex02r <alex.giochi02@gmail.com>
2024-02-13 15:27:05 +0100

    [UPGRADE] Struttura html di comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,10 +1,28 @@
 @extends('layout.mockup')
 
 @section('content')
-    <div class="container">
+    <div class="jumbo">
+        
+    </div>
+    <div class="container py-5">
         <div class="row">
-            <div class="col-12">
-                <h1>Comic</h1>
+            <div class="col-8">
+                {{-- SPESC COMIC --}}
+            </div>
+            <div class="col-4">
+                {{-- ADVERTISEMENT --}}
+            </div>
+        </div>
+    </div>
+    <div class="bg-secondary">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    {{-- talent --}}
+                </div>
+                <div class="col-6">
+                    {{-- specs --}}
+                </div>
             </div>
         </div>
     </div>
