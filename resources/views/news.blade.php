0c6eb0d09d37689f9b8dfbfd33eabf95adde373c
alex02r <alex.giochi02@gmail.com>
2024-02-13 15:05:59 +0100

    [UPGRADE] Convert all page mockup

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,24 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CHARACHTERS</title>
-    @vite('resources/js/app.js')
-</head>
-<body>
-    @include('partials.header')
-    <main class="bg-dark text-white">
-        <div class="container py-5">
-            <div class="row">
-                <div class="col-12 py-5">
-                    <h1> PAGE NEWS</h1>
-                    <h2> in allestimento</h2>
-                </div>
+@extends('layout.mockup')
+
+@section('content')
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-12 py-5">
+                <h1> PAGE NEWS</h1>
+                <h2> in allestimento</h2>
             </div>
         </div>
-    </main>
-    @include('partials.footer')
-</body>
-</html>
+    </div>
+@endsection
